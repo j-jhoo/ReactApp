c8f55f82897f5f7f5a0335b5c437d510d6a432f7
정지후 <loveadsadf1976@gmail.com>
2022-02-14 20:36:31 +0900

    간단한 객체생성 및 map을 사용한 반복

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 import Template from "./components/Template";
+import TodoList from "./components/TodoList";
 
 const App = () => {
-  return <Template>안녕하세요!</Template>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할일 1",
+      checkde: true,
+    },
+    {
+      id: 2,
+      text: "할일 2",
+      checkde: false,
+    },
+    {
+      id: 3,
+      text: "할일 3",
+      checkde: true,
+    },
+  ]);
+
+  return (
+    <Template>
+      <TodoList todos={todos} />
+    </Template>
+  );
 };
 
 export default App;

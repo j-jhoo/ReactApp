9e3dea19fdd9c08de46d3b9daef8f39a27bf6da8
정지후 <loveadsadf1976@gmail.com>
2022-02-15 23:19:13 +0900

    Template css 추가

--- todo/src/components/Template.js
+++ todo/src/components/Template.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Template = ({ children }) => {
   return (
-    <flagment>
-      <div>오늘 할 일 (0)</div>
+    <div className="Template">
+      <div className="title">오늘 할 일 (0)</div>
       <div>{children}</div>
-    </flagment>
+    </div>
   );
 };
 

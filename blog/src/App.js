54823928345cb929c94cf90acb0c47b52191803c
정지후 <loveadsadf1976@gmail.com>
2022-02-18 14:05:19 +0900

    blog 폴더 생성

---
+++ blog/src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+
+const App = () => {
+  const lala = "강남 맛집";
+
+  return (
+    <div className="App">
+      <header className="blackNav">
+        <div>개발 블로그</div>
+      </header>
+
+      <h4>{lala}</h4>
+    </div>
+  );
+};
+export default App;

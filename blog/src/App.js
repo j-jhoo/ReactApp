1bdce618eed27b8dfebf25e166a586382de725c1
정지후 <loveadsadf1976@gmail.com>
2022-02-20 17:48:41 +0900

    버튼생성

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,24 @@
+import React, { useState } from "react";
 import "./App.scss";
 
 const App = () => {
-  const lala = "강남 맛집";
+  const [blogTitle, setBlogTitle] = useState([
+    "리액트 라이브러리",
+    "리액트",
+    "자바스크립트",
+  ]);
+
+  const [good, setGood] = useState(0);
+
+  const upGoodButton = () => {
+    setGood(good + 1);
+  };
+
+  const changeTitleButton = () => {
+    let newTitle = [...blogTitle];
+    newTitle[0] = "뷰 라이브러리";
+    setBlogTitle(newTitle);
+  };
 
   return (
     <div className="App">
@@ -9,8 +26,11 @@ const App = () => {
         <div>개발 블로그</div>
       </header>
       <div className="list">
-        <h3>{lala}</h3>
+        <h3>
+          {blogTitle[0]} <span onClick={upGoodButton}>👍🏻 </span> {good}
+        </h3>
         <p>2월 18일 발행</p>
+        <button onClick={changeTitleButton}>버튼</button>
         <hr />
       </div>
     </div>

4eb4d3847e685202d620af819b3f9246c9075d3f
정지후 <loveadsadf1976@gmail.com>
2022-03-02 22:15:57 +0900

    파일 추가

--- foodBasket/src/components/Meals/Meals.js
+++ foodBasket/src/components/Meals/Meals.js
@@ -1,1 +1,15 @@
-const Meals = 
+import { Fragment } from "react";
+
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;

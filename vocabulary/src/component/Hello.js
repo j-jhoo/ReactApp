31bf374bf8b987f8211ac452e9ae9ece4e65aa7a
정지후 <loveadsadf1976@gmail.com>
2022-03-16 20:58:43 +0900

    다양한 방법의 함수 작성법

--- vocabulary/src/component/Hello.js
+++ vocabulary/src/component/Hello.js
@@ -1,9 +1,28 @@
 const Hello = () => {
+  const showName = () => {
+    console.log("hoo");
+  };
+  const inputText = (text) => {
+    console.log(text);
+  };
   return (
     <div>
       <h1>Hello</h1>
-      <button>Show Name</button>
-      <button>Show Age</button>
+      <button onClick={showName}>Show Name</button>
+      <button
+        onClick={() => {
+          console.log("fkkffk");
+        }}
+      >
+        Show Age
+      </button>
+      <input
+        type="text"
+        onChange={(e) => {
+          const text = e.target.value;
+          inputText(text);
+        }}
+      />
     </div>
   );
 };

9e64ba0b1315a1aa79d1fb4be32ad207b5ddc0b7
정지후 <loveadsadf1976@gmail.com>
2022-03-19 22:57:39 +0900

    react-router v6 *

--- vocabulary/src/App.js
+++ vocabulary/src/App.js
@@ -2,6 +2,7 @@ import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+          {/* React V6부터는 상단에 위치하는 라우터들의 규칙을 모두 확인, 일치하는 라우트가 없는 경우 *로 처리해서 사용 가능함  */}
         </Routes>
       </div>
     </BrowserRouter>

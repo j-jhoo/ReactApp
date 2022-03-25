b911d84dc00f8ca8149441807f072b2d697db421
정지후 <loveadsadf1976@gmail.com>
2022-03-25 23:35:43 +0900

    단어추가 버튼 기능 구현

--- vocabulary/src/App.js
+++ vocabulary/src/App.js
@@ -2,6 +2,7 @@ import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateWord from "./component/CreateWord";
 import EmptyPage from "./component/EmptyPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
           <Route path="*" element={<EmptyPage />} />
           {/* React V6부터는 상단에 위치하는 라우터들의 규칙을 모두 확인, 일치하는 라우트가 없는 경우 *로 처리해서 사용 가능함  */}
         </Routes>

362667c756c9f2b5e2c6c0849ceca4eb51562bf1
정지후 <loveadsadf1976@gmail.com>
2022-03-29 23:15:37 +0900

    3G 환경에서 loading화면 구현

--- vocabulary/src/component/CreateWord.js
+++ vocabulary/src/component/CreateWord.js
@@ -1,35 +1,39 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 export default function CreateWord() {
   const days = useFetch("http://localhost:3001/days");
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   function onSubmit(e) {
     e.preventDefault();
     // console.log(engRef.current.value);
     // console.log(korRef.current.value);
     // console.log(dayRef.current.value);
-
-    fetch(`http://localhost:3001/words/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // useRef 사용 (current.value)
-      body: JSON.stringify({
-        eng: engRef.current.value,
-        kor: korRef.current.value,
-        day: dayRef.current.value,
-        isDone: false,
-      }),
-    }).then((res) => {
-      if (res.ok) {
-        alert("생성이 완료 되었습니다");
-        navigate(`/day/${dayRef.current.value}`);
-      }
-    });
+    if (!isLoading) {
+      setIsLoading(true);
+      fetch(`http://localhost:3001/words/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // useRef 사용 (current.value)
+        body: JSON.stringify({
+          eng: engRef.current.value,
+          kor: korRef.current.value,
+          day: dayRef.current.value,
+          isDone: false,
+        }),
+      }).then((res) => {
+        if (res.ok) {
+          alert("생성이 완료 되었습니다");
+          navigate(`/day/${dayRef.current.value}`);
+          setIsLoading(false);
+        }
+      });
+    }
   }
 
   const engRef = useRef(null);
@@ -56,7 +60,13 @@ export default function CreateWord() {
           ))}
         </select>
       </div>
-      <button>저장</button>
+      <button
+        style={{
+          opacity: isLoading ? 0.3 : 1,
+        }}
+      >
+        {isLoading ? "Saving" : "저장"}
+      </button>
     </form>
   );
 }
